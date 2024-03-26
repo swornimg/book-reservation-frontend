b3dd4586d7b268563eb5c0f3804ab02726a6437a
Krishnna-Gurung <32131052@student.uwl.ac.uk>
2024-03-26 18:03:18 +0000

    Bookreservation frontend started
    Book Reservation FrontEnd Project created. (Start of the Project)

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Routes, Route, Link } from 'react-router-dom';
+import './App.css';
+import Home from './pages/home/Home';
+import Layout from './components/Layout';
+
+function App() {
+
+  return (
+    <div>
+      <Home />
+    </div>
+  )
+}
+
+export default App
