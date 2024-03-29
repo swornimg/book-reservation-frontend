c17968c77ef9b30c98315fb0eb77e0c8fb9bb4b2
Krishnna-Gurung <32131052@student.uwl.ac.uk>
2024-03-29 16:42:49 +0000

    Routes and folder configuration

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,39 @@
-import React from "react";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-import "./App.css";
-import Home from "./pages/home/Home.jsx";
-import Layout from "./components/Layout.jsx";
-import Login from "./pages/login/Login.jsx";
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import './App.css'
+import Home from './pages/home/Home.jsx';
+import Layout from './components/Layout.jsx';
+import Login from './pages/login/Login.jsx';
+import Discover from './pages/discover/Discover.jsx';
+import Reading from './pages/reading/Reading.jsx';
+import Wanttoread from './pages/wanttoread/Wanttoread.jsx';
+import Completed from './pages/completed/Completed.jsx';
+import Profile from './pages/profile/Profile.jsx';
+import FAQs from './pages/faqs/FAQs.jsx';
+import Contactus from './pages/contactus/Contactus.jsx';
 
 function App() {
+
   return (
     <div>
       <BrowserRouter>
-        <Layout />
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route element={<Layout />}>
+            <Route path="/" element={<Home />} />
+            <Route path="/discover" element={<Discover />} />
+            <Route path="/reading" element={<Reading />} />
+            <Route path="/wanttoread" element={<Wanttoread />} />
+            <Route path="/completed" element={<Completed />} />
+            <Route path="/myprofile" element={<Profile />} />
+            <Route path="/faqs" element={<FAQs />} />
+            <Route path="/contactus" element={<Contactus />} />
+          </Route>
           <Route path="/login" element={<Login />} />
         </Routes>
       </BrowserRouter>
+
     </div>
-  );
+  )
 }
 
 export default App;
